32c56f299ddccaa4c6d43bad66778e5674cad6c7
Amihere Theophilus Junior <snrraptopack@gmail.com>
2026-02-09 08:58:37 +0000

    Start of rewrite in Rust

--- javascript/keys.ts
+++
@@ -1,6 +0,0 @@
-
-export const TempKey = "AIzaSyCxW2TQt4_2AAs2dbm6v3N765bz_gnFvN4"
-export const TempKey1 = "AIzaSyAEr1WOYe-UkqSif2cgD5muhHbbx2lOwfU"
-
-export const kimi = "sk-BtBvmXWENqpZqggZF7Zi2x4pYvT7Cl5TfJb2nGsBAuEKMwQF"
-export const openAiKey = "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"
