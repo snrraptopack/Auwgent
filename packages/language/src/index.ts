be1d1d1251d5c935df8f67245ec9c047232c3a7a
Amihere Theophilus Junior <snrraptopack@gmail.com>
2026-03-13 01:56:18 +0000

    initial cicd integration

--- packages/language/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './auwgent-module.js';
-export * from './auwgent-validator.js';
-export * from './generated/ast.js';
-export * from './generated/grammar.js';
-export * from './generated/module.js';
--- packages/language/src/type-system/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types.js';
-export * from './constraints.js';
-export * from './env.js';
-export * from './unification.js';
-export * from './diagnostics.js';
-export * from './inference.js';
-export * from './checker.js';
